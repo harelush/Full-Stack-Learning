fe49c7b9efdb115def16d9ef82fec5bca5817c4a
harel <harel1999chen@gmail.com>
2024-04-22 13:02:11 +0300

    added variables

---
+++ 01- Variables/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int example = 55;
+        int another = example;
+        System.out.println(example);
+        System.out.println(another);
+
+        System.out.println("-----------");
+
+        example = 77;
+        another = 66;
+        System.out.println(example);
+        System.out.println(another);
+    }
+}
---
+++ 01- Variables/main.js
@@ -0,0 +1,11 @@
+let example = 55;
+let another = example;
+
+console.log(example);
+console.log(another);
+
+example = 77;
+console.log(example);
+another = 66;
+
+console.log(another);
