e13ec7559d7a0a2e2910e42e1b432762c27e1648
harel <harel1999chen@gmail.com>
2024-04-28 01:28:04 +0300

    added third

---
+++ 01- Variables/Questions/ThirdQuestion.java
@@ -0,0 +1,12 @@
+public class ThirdQuestion {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello world");
+        int x = 5;
+        final double y = 6;
+        short z = 7;
+
+        x = 55;
+        z = 77;
+
+    }
+}
---
+++ 01- Variables/Questions/thirdQuestion.js
@@ -0,0 +1,7 @@
+let x = 5;
+const y = 6;
+let z = 7;
+
+x = 55;
+z = 77;
+
