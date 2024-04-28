b03ed2d86ba0f89095a21887707a4d4e01226486
harel <harel1999chen@gmail.com>
2024-04-29 02:02:58 +0300

    added functions example 1

---
+++ 02-Functions/FunctionExample2.java
@@ -0,0 +1,9 @@
+public class FunctionExample2 {
+    public static void main(String[] args) throws Exception {
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("hello how are you?");
+    }
+}
