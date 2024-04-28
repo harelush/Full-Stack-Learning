a2d6386c0451868056416cbb60605d330d770478
harel <harel1999chen@gmail.com>
2024-04-29 02:01:13 +0300

    added functions

---
+++ 02-Functions/FunctionExample.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+
+
+
+
+    public static void greet(){
+        
+    }
+
+    
+}
