47b25e270655483e24809263d9f9455c8fafa5f5
harel <harel1999chen@gmail.com>
2024-05-03 02:23:20 +0300

    added parameters examples

---
+++ 02-Functions/Parameters.java
@@ -0,0 +1,13 @@
+public class Parameters {
+    public static void main(String[] args) {
+        add(5,6);
+        add(30,60); 
+        add(50,50);       
+    }
+
+    public static void add(int a, int b ){
+        System.out.println("the add is: ");
+        System.out.println(a + b);
+    }
+    
+}
---
+++ 02-Functions/parameters.js
@@ -0,0 +1,16 @@
+function sub(a, b) {
+    console.log("the result of the subtract is:");
+    console.log(b - a);
+}
+
+function add(a, b) {
+    console.log("the result of the add is:");
+    console.log(b + a);
+}
+
+sub(10,5);
+sub(111,66);
+sub(9,6);
+
+add(70,30);
+add(10,20);
