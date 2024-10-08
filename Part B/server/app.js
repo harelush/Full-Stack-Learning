30468d4e5e24bb9e71751316d6c7b5e06a01ade3
harel <harel1999chen@gmail.com>
2024-10-09 01:51:15 +0300

    added dotenv

--- Part B/server/app.js
+++ Part B/server/app.js
@@ -1,12 +1,13 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
-const port = 3000
 const routes = require('./routes/index');
 
 app.use(express.json());
 
 app.use('/api', routes);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
--- Part B/server/config/postgres.js
+++ Part B/server/config/postgres.js
@@ -2,11 +2,11 @@ const { query } = require('express');
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'postgres',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 pool.on('connect', () => {
