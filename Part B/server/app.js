2eb4a8e9cf400fa96c7a1f2fb0191ca321f374e3
harel <harel1999chen@gmail.com>
2024-10-09 02:30:47 +0300

    added mongoose

--- Part B/server/app.js
+++ Part B/server/app.js
@@ -3,10 +3,17 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const routes = require('./routes/index');
+const mongoose = require('./config/mongodb');
 
 app.use(express.json());
 
 app.use('/api', routes);
+
+app.use((req, res, next)=> {
+    res.status(404).json({
+        error: 'Route Not Found',
+    })
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
---
+++ Part B/server/config/mongodb.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Error while connecting to MongoDB ', err);
+});
+
+module.exports = mongoose;
