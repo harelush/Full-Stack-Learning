f42d65362f3827cd2cf7695ff831c1de172c9151
harelush <61940002+harelush@users.noreply.github.com>
2024-10-20 01:45:55 +0300

    Update productController.js

--- Part B/server/controllers/productController.js
+++ Part B/server/controllers/productController.js
@@ -5,6 +5,19 @@ const getAllProducts = async (req, res) => {
     res.json(products);
 }
 
+const getProductById = async (req, res) => {
+    const id = req.params.id;
+    const result = await productModel.getProductById(id);
+
+    if(!result) {
+        res.status(404).send('Product not found');
+    }
+
+    res.json(result);
+}
+
+
 module.exports = { 
     getAllProducts,
+    getProductById,
 }
