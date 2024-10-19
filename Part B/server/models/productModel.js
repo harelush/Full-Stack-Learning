079c048c6f1f2a9e87c7d25f204ea125206003d0
harelush <61940002+harelush@users.noreply.github.com>
2024-10-20 01:45:39 +0300

    Update productModel.js

--- Part B/server/models/productModel.js
+++ Part B/server/models/productModel.js
@@ -1,11 +1,19 @@
 const postgres = require('../config/postgres');
 
 async function getAllProducts() {
-    const QUERY = 'SELECT product_id as id, image_src, product_name, price FROM public.products';
+    const QUERY = 'SELECT product_id AS id, image_src AS image, product_name AS name, price FROM products';
     const result = await postgres.query(QUERY);
+
     return result.rows;
+}
+
+async function getProductById(id) {
+    const QUERY = 'SELECT product_id AS id, image_src AS image, product_name AS name, price, description FROM products where product_id = $1';
+    const result = await postgres.query(QUERY, [id]);
+    return result.rows[0];
 }
 
 module.exports = {
     getAllProducts,
+    getProductById,
 }
